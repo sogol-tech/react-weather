ce31946caa26b4149c3451ec85483f189b629d9e
sogol-tech <afsharsogol@icloud.com>
2021-08-18 10:59:18 +0100

    Added bootsrap.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Weather App</p>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/sogol-tech" target="_blank">
-          Sogol Afshar
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/sogol-tech/react-weather.git"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github{" "}
-        </a>
-        and hosted on{" "}
-        <a href="https://happy-noyce-124f1d.netlify.app/" target="-blank">
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <p>Weather App</p>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/sogol-tech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sogol Afshar
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/sogol-tech/react-weather.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github{" "}
+          </a>
+          and hosted on{" "}
+          <a href="https://happy-noyce-124f1d.netlify.app/" target="-blank">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
