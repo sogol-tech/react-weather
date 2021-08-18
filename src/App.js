a493ac8c21e027b90df136806a3de5a7a26119e5
sogol-tech <afsharsogol@icloud.com>
2021-08-18 10:44:10 +0100

    Added links to Github and Netlify.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <p>Weather App</p>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/sogol-tech" target="_blank">
+          Sogol Afshar
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/sogol-tech/react-weather.git"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github{" "}
+        </a>
+        and hosted on{" "}
+        <a href="https://happy-noyce-124f1d.netlify.app/" target="-blank">
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
