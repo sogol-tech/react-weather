934bfb1f7e7201c4bb0840ae100f5a85413bb8a5
sogol-tech <afsharsogol@icloud.com>
2021-08-27 16:50:50 +0100

    Icon change.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,27 +1,46 @@
+import React from "react";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiNightCloudy,
+  WiCloudyWindy,
+  WiNightAltCloudyWindy,
+  WiCloudyGusts,
+  WiNightAltCloudyGusts,
+  WiDayRain,
+  WiNightAltRain,
+  WiDayRainMix,
+  WiNightAltRainMix,
+  WiDayThunderstorm,
+  WiNightAltThunderstorm,
+  WiDaySnowWind,
+  WiNightAltSnowWind,
+  WiWindy,
+  WiStrongWind,
+} from "weather-icons-react";
+
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "01d": <WiDaySunny size={24} color="black" />,
-    "01n": <WiNightClear size={24} color="black" />,
-    "02d": <WiDayCloudy size={24} color="black" />,
-    "02n": <WiNightCloudy size={24} color="black" />,
-    "03d": <WiCloudyWindy size={24} color="black" />,
-    "03n": <WiNightAltCloudyWindy size={24} color="black" />,
-    "04d": <WiCloudyGusts size={24} color="black" />,
-    "04n": <WiNightAltCloudyGusts size={24} color="black" />,
-    "09d": <WiDayRain size={24} color="black" />,
-    "09n": <WiNightAltRain size={24} color="black" />,
-    "10d": <WiDayRainMix size={24} color="black" />,
-    "10n": <WiNightAltRainMix size={24} color="black" />,
-    "11d": <WiDayThunderstorm size={24} color="black" />,
-    "11n": <WiNightAltThunderstorm size={24} color="black" />,
-    "13d": <WiDaySnowWind size={24} color="black" />,
-    "13n": <WiNightAltSnowWind size={24} color="black" />,
-    "50d": <WiWindy size={24} color="black" />,
-    "50n": <WiStrongWind size={24} color="black" />,
+    "01d": <WiDaySunny size={40} color="black" />,
+    "01n": <WiNightClear size={40} color="black" />,
+    "02d": <WiDayCloudy size={40} color="black" />,
+    "02n": <WiNightCloudy size={40} color="black" />,
+    "03d": <WiCloudyWindy size={40} color="black" />,
+    "03n": <WiNightAltCloudyWindy size={40} color="black" />,
+    "04d": <WiCloudyGusts size={40} color="black" />,
+    "04n": <WiNightAltCloudyGusts size={40} color="black" />,
+    "09d": <WiDayRain size={40} color="black" />,
+    "09n": <WiNightAltRain size={40} color="black" />,
+    "10d": <WiDayRainMix size={40} color="black" />,
+    "10n": <WiNightAltRainMix size={40} color="black" />,
+    "11d": <WiDayThunderstorm size={40} color="black" />,
+    "11n": <WiNightAltThunderstorm size={40} color="black" />,
+    "13d": <WiDaySnowWind size={40} color="black" />,
+    "13n": <WiNightAltSnowWind size={40} color="black" />,
+    "50d": <WiWindy size={40} color="black" />,
+    "50n": <WiStrongWind size={40} color="black" />,
   };
-  return class Weather extends React.Component {
-    render() {
-      return codeMapping[props.code];
-    }
-  };
+
+  return codeMapping[props.code];
 }
